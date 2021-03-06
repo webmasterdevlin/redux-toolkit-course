2be9b9cefdbb5a471de47c99ebc354f1d63fac21
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-03-06 08:18:02 +0100

    updated

--- src/test-utils/testing-library-utils.tsx
+++ src/test-utils/testing-library-utils.tsx
@@ -1,37 +1,37 @@
 import React from "react";
 import { render as rtlRender } from "@testing-library/react";
 import { Provider } from "react-redux";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter } from "react-router-dom";
+
 // Import your own reducer
 import { initialState as reducerInitialState } from "../features/antiHeroes/antiHeroSlice";
 import { configureAppStore } from "../store/configureStore";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { BrowserRouter } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
 
-function render(
+const render = (
   ui,
   {
     initialState = reducerInitialState,
     store = configureAppStore(),
     ...renderOptions
   } = {}
-) {
-  function Wrapper({ children }) {
-    return (
-      <Provider store={store}>
-        <CssBaseline>
-          <BrowserRouter>
-            <>
-              <NavigationBar />
-              {children}
-            </>
-          </BrowserRouter>
-        </CssBaseline>
-      </Provider>
-    );
-  }
+) => {
+  const Wrapper = ({ children }) => (
+    <Provider store={store}>
+      <CssBaseline>
+        <BrowserRouter>
+          <>
+            <NavigationBar />
+            {children}
+          </>
+        </BrowserRouter>
+      </CssBaseline>
+    </Provider>
+  );
+
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
-}
+};
 
 // re-export everything
 export * from "@testing-library/react";

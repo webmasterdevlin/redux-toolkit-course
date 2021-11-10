0e631426f72c9bbba8caf9181aa834f98f22a044
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-10 15:55:58 +0100

    updated

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,25 +1,25 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { forceReducerReload } from 'redux-injectors';
-import { createReducer } from './reducers';
+import { configureStore } from "@reduxjs/toolkit";
+import { forceReducerReload } from "redux-injectors";
+import { createReducer } from "./reducers";
 
 export function configureAppStore() {
   const store = configureStore({
     reducer: createReducer(),
-    middleware: [
+    middleware: (getDefaultMiddleware) => [
       ...getDefaultMiddleware({
         serializableCheck: false,
       }),
     ],
     devTools:
       /* istanbul ignore next line */
-      process.env.NODE_ENV !== 'production' ||
+      process.env.NODE_ENV !== "production" ||
       process.env.PUBLIC_URL.length > 0,
   });
 
   // Make reducers hot reloadable, see http://mxs.is/googmo
   /* istanbul ignore next */
   if (module.hot) {
-    module.hot.accept('./reducers', () => {
+    module.hot.accept("./reducers", () => {
       forceReducerReload(store);
     });
   }

5101eb240ff8782868f70f5b61767af5d039475e
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-11-11 17:56:53 +0100

    update

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,9 +1,6 @@
 // @ts-check
 ///<reference path="../global.d.ts" />
 /// <reference types="cypress"/>
-import "@cypress/code-coverage/support";
-import "@percy/cypress";
-import "@bahmutov/cy-api/support";
 import "@testing-library/cypress/add-commands";
 import { v4 as uuidv4 } from "uuid";
 
